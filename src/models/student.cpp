fbd3fc6f31e88619fc28c8466a968677c484d7f7
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-01-21 17:28:58 -0300

    feat/done-controller-student

--- src/models/student.cpp
+++ src/models/student.cpp
@@ -1,20 +1,37 @@
 #include "Student.h"
 
-Student::Student(int id, const std::string& name) : id(id), name(name), absences(0) {}
+Student::Student(int id, const std::string &name, const std::string &adressName) : id(id), name(name), absences(0) adressName(adressName) {}
 
-int Student::getId() const {
+int Student::getId() const
+{
     return id;
 }
 
-std::string Student::getName() const {
+std::string Student::getName() const
+{
     return name;
 }
 
-int Student::getAbsences() const {
+int Student::getAbsences() const
+{
     return absences;
 }
 
-void Student::addAbsence() {
+void Student::addAbsence()
+{
     absences++;
 }
 
+// void Student::addAdress()
+// {
+//     return adressName;
+// }
+std::string Student::getAdress() const
+{
+    return adressName;
+}
+
+void Student::setAdress(const std::string &newAdress)
+{
+    adressName = newAdress;
+}
--- src/models/student.h
+++ src/models/student.h
@@ -3,19 +3,24 @@
 
 #include <string>
 
-class Student {
+class Student
+{
 private:
     int id;
     std::string name;
     int absences; // Quantidade de faltas
+    std::string adressName;
 
 public:
     // Nos models teremos somente os modelos, então terão somente os atributos da classe como privado e os setters and getters
-    Student(int id, const std::string& name);
+    Student(int id, const std::string &name, const std::string adressName);
     int getId() const;
     std::string getName() const;
     int getAbsences() const;
+    //setters
     void addAbsence(); // Incrementa as faltas --> função que vai estar no controller, tudo em relação a lógica é no controller, isso foi meramente um exemplo
+    std::string setAdress();
+    // void updateAdress();  !!!!!!!!!!!!!!!11111
 };
 
 #endif
